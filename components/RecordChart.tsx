18ad6c990c1b0cdeadac188719f4dd0b590c8945
mwaraem <mwaraem@gmail.com>
2025-11-25 13:35:34 +0300

    Added Expense Statistics Component

--- components/RecordChart.tsx
+++ components/RecordChart.tsx
@@ -1,4 +1,4 @@
-import getRecords from '@/app/actions/getRecords';
+import getRecords from '@/app/actions/getRecord';
 import BarChart from './BarChart'; // Ensure BarChart.tsx or BarChart.jsx exists in the same directory
 
 const RecordChart = async () => {
